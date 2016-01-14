83c4ee58a2aac25e22098e5378c82e38ebd783c5
kate hall and Tim Durward <kwarrenhall@gmail.com>
2016-01-14 14:23:02 -0800

    add modal & accordion structure

---
+++ js/master.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+$(".btn").click(function(){
+
+  $(".show-preview").toggle();
+  $(".show-code").toggle();
+
+
+});
+
+
+});
+
+
+// Markdown
+// Block elements
+// Inline elements
+// CSS
+// Bootstrap
+// Divs and spans
+// Floats
+// Box model
+// Cascading styles
+// Grid system
+// Responsive design
+// JavaScript (JS) arithmetic
+// JS variables
+// JS methods
+// JS strings
+// JS functions
+// jQuery
+// DOM manipulation and traversal
+// Forms
+// Attributes
+// Branching
+// Arrays
+// Looping
